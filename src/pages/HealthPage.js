4f8e282356fa5f3ca0682659c4700e4baa4dded2
sok5188 <sok5188@gmail.com>
2024-03-25 21:53:31 +0900

    add deploy script

--- src/pages/HealthPage.js
+++ src/pages/HealthPage.js
@@ -5,7 +5,7 @@ import React from 'react';
 function HealthPage() {
     return (
         <div>
-            <h1>Welcome to the Health Page!</h1>
+            <h1>Welcome to the Health Page!!!</h1>
             <h3>React server is alive and routing is Fine !</h3>
         </div>
     );
