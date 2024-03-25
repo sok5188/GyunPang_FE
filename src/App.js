dc5e6f4afa0461a6371a0a270e01ef99b475a568
sok5188 <sok5188@gmail.com>
2024-03-25 20:13:06 +0900

    add health page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HealthPage from "./pages/HealthPage";
+import DefaultPage from "./pages/DefaultPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/health" element={<HealthPage />} />
+                <Route path="/" element={<DefaultPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/HealthPage.js
@@ -0,0 +1,14 @@
+// HealthPage.js
+
+import React from 'react';
+
+function HealthPage() {
+    return (
+        <div>
+            <h1>Welcome to the Health Page!</h1>
+            {/* 원하는 환영 메시지 및 컨텐츠를 추가할 수 있습니다. */}
+        </div>
+    );
+}
+
+export default HealthPage;
