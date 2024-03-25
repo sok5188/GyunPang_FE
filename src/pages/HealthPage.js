1607f19a5fe42da6276dac7002b0da7750216eda
sok5188 <sok5188@gmail.com>
2024-03-25 21:02:57 +0900

    add health page

--- src/pages/HealthPage.js
+++ src/pages/HealthPage.js
@@ -6,7 +6,7 @@ function HealthPage() {
     return (
         <div>
             <h1>Welcome to the Health Page!</h1>
-            {/* 원하는 환영 메시지 및 컨텐츠를 추가할 수 있습니다. */}
+            <h3>React server is alive and routing is Fine !</h3>
         </div>
     );
 }
