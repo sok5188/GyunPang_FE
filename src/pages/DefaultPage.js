24714032932c6ea23581998fdbf687d28a7c7cf2
sok5188 <sok5188@gmail.com>
2024-03-25 22:32:11 +0900

    add deploy script

--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -9,7 +9,7 @@ function DefaultPage() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/App.js</code> and save to reload.123123
                 </p>
                 <a
                     className="App-link"
