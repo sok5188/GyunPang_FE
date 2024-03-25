caec55e8dc5af33b91baaad5d9b6200aee6e7f19
sok5188 <sok5188@gmail.com>
2024-03-25 21:26:12 +0900

    add deploy script

---
+++ fe_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IS_GREEN=$(cat /etc/nginx/conf.d/fe-route.inc | grep green) # 현재 실행중인 App이 blue인지 확인합니다.
+
+if [ -z $IS_GREEN  ];then # blue라면
+
+  echo "### BLUE => GREEN ###"
+
+  cp /etc/nginx/conf.d/fe-route-green /etc/nginx/conf.d/fe-route.inc
+
+  sudo nginx -s reload
+
+else
+  echo "### GREEN => BLUE ###"
+
+    cp /etc/nginx/conf.d/fe-route-blue /etc/nginx/conf.d/fe-route.inc
+
+    sudo nginx -s reload
+fi
